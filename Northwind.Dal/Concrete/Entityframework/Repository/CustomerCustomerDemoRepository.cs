4c939650556445106a28a92f924a1df6adbc5978
Qeqibo <tarhanubeyt@gmail.com>
2021-12-25 09:57:15 +0300

    Ödev2

---
+++ Northwind.Dal/Concrete/Entityframework/Repository/CustomerCustomerDemoRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Northwind.Entity.Models;
+using System.Linq;
+
+namespace Northwind.Dal.Concrete.Entityframework.Repository
+{
+    public class CustomerCustomerDemoRepository : GenericRepository<CustomerCustomerDemo>, ICustomerCustomerDemo
+    {
+        public CustomerCustomerDemoRepository(DbContext context) : base(context)
+        {
+
+        }
+
+        public IQueryable OrderReport(int orderId)
+        {
+            return dbset.AsQueryable();
+        }
+    }
+}
---
+++ Northwind.Entity/Mapper/MappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Northwind.Entity.Dto;
+using Northwind.Entity.Models;
+
+namespace Northwind.Entity.Mapper
+{
+    public class MappingProfile:Profile
+    {
+
+        public MappingProfile()
+        {
+            CreateMap<Customer, DtoCustomer>().ReverseMap();
+            
+            CreateMap<Order, DtoOrder>().ReverseMap();
+            CreateMap<Category, DtoCategory>().ReverseMap();
+            CreateMap<CustomerCustomerDemo, DtoCustomerCustomerDemo>().ReverseMap();
+            CreateMap<CustomerDemographic, DtoCustomerDemographic>().ReverseMap();
+            CreateMap<Employee, DtoEmployee>().ReverseMap();
+            CreateMap<EmployeeTerritory, DtoEmployeeTerritory>().ReverseMap();
+            CreateMap<OrderDetail, DtoOrderDetail>().ReverseMap();
+            CreateMap<Product, DtoProduct>().ReverseMap();
+            CreateMap<Region, DtoRegion>().ReverseMap();
+            CreateMap<Shipper, DtoShipper>().ReverseMap();
+            CreateMap<Supplier, DtoSupplier>().ReverseMap();
+            CreateMap<Territory, DtoTerritory>().ReverseMap();
+        }
+    }
+}
